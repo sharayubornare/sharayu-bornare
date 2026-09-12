553cbbe89a70553d663d4b3ff0ce87f44873473f
sharayu bornare <bornaresharayu@gmail.com>
2026-09-12 03:32:48 -0700

    Add Terraform AWS EC2 and Docker portfolio

---
+++ terraform/main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Find the latest Ubuntu 24.04 LTS AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# EC2 instance
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  key_name                    = "terraform-devops-key"
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y docker.io
+    systemctl enable docker
+    systemctl start docker
+    usermod -aG docker ubuntu
+    docker run -d --name devops-nginx -p 80:80 nginx:alpine
+  EOF
+
+  tags = {
+    Name      = "terraform-devops-web-server"
+    Project   = "AWS-DevOps-Portfolio"
+    ManagedBy = "Terraform"
+  }
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnets from the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Security Group for the web server
+resource "aws_security_group" "web" {
+  name        = "terraform-devops-web-sg"
+  description = "Allow HTTP and SSH access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name      = "terraform-devops-web-sg"
+    Project   = "AWS-DevOps-Portfolio"
+    ManagedBy = "Terraform"
+  }
+}
+
+# Get the default VPC
